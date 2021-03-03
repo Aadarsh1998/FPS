4071dd6e51690b9ee8b1d656693c29c1b81d45c3
DESKTOP-Aadarsh <aadarshkomera@gmail.com>
2021-03-03 15:53:12 +0530

    BulletSpawn & PlayerRotation done

---
+++ Assets/BulletDestroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDestroy : MonoBehaviour
+{
+    float time;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = time + Time.deltaTime;
+        if(time>3.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
